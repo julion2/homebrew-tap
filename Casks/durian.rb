51462c3951b3cb26b72a58bc87f8b14e640ddb31
Julian Schenker <julian@habric.com>
2026-03-19 23:27:58 +0100

    feat: add durian formula (CLI) and cask (GUI)

---
+++ Casks/durian.rb
@@ -0,0 +1,11 @@
+cask "durian" do
+  version "0.1.1"
+  sha256 "PLACEHOLDER_GUI"
+
+  url "https://github.com/julion2/Durian/releases/download/v#{version}/Durian-#{version}.zip"
+  name "Durian"
+  desc "macOS email client for power users"
+  homepage "https://github.com/julion2/Durian"
+
+  app "Durian.app"
+end
---
+++ Formula/durian.rb
@@ -0,0 +1,28 @@
+class Durian < Formula
+  desc "Fast email client CLI - IMAP sync, SQLite store, HTTP API"
+  homepage "https://github.com/julion2/Durian"
+  version "0.1.1"
+  license "MIT"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/julion2/Durian/releases/download/v#{version}/durian-#{version}-darwin-arm64.tar.gz"
+      sha256 "PLACEHOLDER_ARM64"
+    else
+      url "https://github.com/julion2/Durian/releases/download/v#{version}/durian-#{version}-darwin-amd64.tar.gz"
+      sha256 "PLACEHOLDER_AMD64"
+    end
+  end
+
+  def install
+    if Hardware::CPU.arm?
+      bin.install "durian-darwin-arm64" => "durian"
+    else
+      bin.install "durian-darwin-amd64" => "durian"
+    end
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/durian --version")
+  end
+end
